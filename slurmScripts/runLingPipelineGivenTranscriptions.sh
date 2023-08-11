41a29a3500dfe0f9d695547df3a08a352e2bac70
lokesh12345678910 <44538396+lokesh12345678910@users.noreply.github.com>
2023-08-11 14:41:21 -0500

    Update runLingPipelineGivenTranscriptions.sh

--- slurmScripts/runLingPipelineGivenTranscriptions.sh
+++ slurmScripts/runLingPipelineGivenTranscriptions.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # -----------------------------------------------
-# SLURM script to derive the final RACS acoustic feature set on a directory of audio files 
+# SLURM script to derive the final RACS linguistic feature set on a directory of text files 
 # ----------------------------------------------
-#SBATCH -J AcousticPipelineFirstRun_Spanish       #name of JOb
-#SBATCH -o AcousticPipelineFirstRun_Spanish.o%j   #name of std output file 
-#SBATCH -e AcousticPipelineFirstRun_Spanish.e%jn  # name of stderr output file
+#SBATCH -J LingPipelineGivenTranscriptions_Trial       #name of JOb
+#SBATCH -o LingPipelineGivenTranscriptions_Trial.o%j   #name of std output file 
+#SBATCH -e LingPipelineGivenTranscriptions_Trial.e%jn  # name of stderr output file
 #SBATCH -p normal           
 #SBATCH -N 1
 #SBATCH -n 1
@@ -13,5 +13,5 @@
 #SBATCH --mail-type=end
 CUDA_VISIBLE_DEVICES=0 
 conda activate racs
-#python tacc_AcousticPipelineWithTrimming.py inputDirectory outputName rVADPath 
+#Format of command, see example below: python tacc_EnglishLingPipelineGivenTranscriptions.py inputDirectory outputName racsAWSPipelineForMADRandARTLab/data/end2end/static/LingFeatData/ 
 python /work/09424/smgrasso1/ls6/nonRACSPythonScripts/tacc_EnglishLingPipelineGivenTranscriptions.py /work/09424/smgrasso1/ls6/trialRuns/nfv_UT_transcriptions/ nfvUTTrialLingFeats /work/09424/smgrasso1/ls6/racsAWSPipelineForMADRandARTLab/data/end2end/static/LingFeatData/
